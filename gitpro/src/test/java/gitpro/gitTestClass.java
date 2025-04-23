efd405b3618d42526c20cfc4174d9690fa3feb97
User <User@192.168.1.8>
2025-04-23 18:04:18 +0530

    First commit

---
+++ gitpro/src/test/java/gitpro/gitTestClass.java
@@ -0,0 +1,12 @@
+package gitpro;
+
+public class gitTestClass {
+
+public static void main(String[] args) {
+	System.out.println("gitTC");
+	System.out.println("gitTC");
+	System.out.println("gitTC");
+	System.out.println("gitTC");
+	System.out.println("gitTC");
+}
+}
